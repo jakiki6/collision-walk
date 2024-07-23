c651be520f5afd17a5773f680c7f50ee901022b7
Jakob Kirsch <jakob.kirsch@web.de>
2024-07-23 07:10:44 +0200

    Refactoring and adding debugging messages

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
+#define DEBUG 0
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <bsd/stdlib.h>
+
+#if DEBUG
+    #define debug printf
+#else
+    #define debug(...)
+#endif
 
 static inline uint64_t f(uint64_t x) {
     x ^= (x >> 33) | (x << 31);
@@ -21,37 +29,49 @@ void solve(uint64_t t, uint64_t a, uint64_t b) {
     uint64_t *t1 = malloc(c * sizeof(uint64_t));
     uint64_t *t2 = malloc(c * sizeof(uint64_t));
 
+    debug("Solving two trails: %016llx %016llx -> %016llx\n", a, b, t);
+
+    debug("Generating trail for %016llx\n", a);
     while (a != t) {
         t1[i1] = a;
         i1++;
         if (i1 >= c) {
             c <<= 1;
+            debug("Reallocating to %llu bytes\n", c * sizeof(uint64_t));
             t1 = realloc(t1, c * sizeof(uint64_t));
         }
         a = f(a);
     }
+    debug("Finished trail with length of %llu\n", i1);
 
     c = 65536;
+    debug("Generating trail for %016llx\n", b);
     while (b != t) {
         t2[i2] = b;
         i2++;
         if (i2 >= c) {
             c <<= 1;
+            debug("Reallocating to %llu bytes\n", c * sizeof(uint64_t));
             t2 = realloc(t2, c * sizeof(uint64_t));
         }   
         b = f(b);
     }
+    debug("Finished trail with length of %llu\n", i2);
 
     i1--;
     i2--;
+    debug("Finding divergence in trails\n");
     while (1) {
         if (t1[i1] == t2[i2]) {
             i1--;
             i2--;
         } else {
-            printf("%16llx and %16llx hash to %16llx\n", t1[i1], t2[i2], f(t1[i1]));
+            printf("%016llx and %016llx hash to %016llx\n", t1[i1], t2[i2], f(t1[i1]));
 
             assert(f(t1[i1]) == f(t2[i2]));
+
+            free(t1);
+            free(t2);
             return;
         }
     }
@@ -60,15 +80,22 @@ void solve(uint64_t t, uint64_t a, uint64_t b) {
 void sieve(uint64_t x) {
     int c = 65536;
     int i = 0;
+    int s = 0;
     uint64_t *d = malloc(c * sizeof(uint64_t));
 
+    debug("Sieving for %016llx\n", x);
     while (1) {
         x = f(x);
+        s++;
 
         if ((x >> 48) == 0) {
             for (int j = 0; j < i; j++) {
                 if (x == d[j]) {
-                    solve(x, d[j - 1], d[i - 1]);
+                    uint64_t a = d[j - 1];
+                    uint64_t b = d[i - 1];
+                    free(d);
+                    debug("Found hit after %llu steps and %llu points\n", s, i);
+                    solve(x, a, b);
                     return;
                 }
             }
@@ -77,6 +104,7 @@ void sieve(uint64_t x) {
             i++;
             if (i >= c) {
                 c <<= 1;
+                debug("Reallocating to %llu bytes\n", c * sizeof(uint64_t));
                 d = realloc(d, c * sizeof(uint64_t));
             }
         }
@@ -85,9 +113,7 @@ void sieve(uint64_t x) {
 
 int main() {
     uint64_t x;
+    arc4random_buf(&x, sizeof(uint64_t));
 
-    while (1) {
-        arc4random_buf(&x, sizeof(uint64_t));
-        sieve(x);
-    }
+    sieve(x);
 }
